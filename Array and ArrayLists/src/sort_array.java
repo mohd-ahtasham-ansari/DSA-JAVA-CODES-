aa2a671ef3f56ec9719db402d09a4f767dfdf853
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-31 19:16:50 +0530

    intial array questions

---
+++ Array and ArrayLists/src/sort_array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sort_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i <n ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+      for (int i = 0; i < arr.length; i++) {
+          System.out.print(arr[i] + " ");
+      }
+    }
+}
