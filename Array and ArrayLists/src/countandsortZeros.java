cbf30b8ee6a017032cb13fb321960428a20f31a9
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-09-17 15:32:37 +0530

    rotation of array

--- Array and ArrayLists/src/countandsortZeros.java
+++ Array and ArrayLists/src/countandsortZeros.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
+//segregation of 0's and 1's'
 public class countandsortZeros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
         int n = sc.nextInt();
+        System.out.println("Enter the elements of the array {[( only zeros and ones )]}: ");
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -13,12 +16,13 @@ public class countandsortZeros {
             if (arr[i] == 0) {
                 count++;
             }
+        }
             for (int j = 0; j <count ; j++) {
                 arr[j]=0;
             }
             for (int j = count; j <n ; j++) {
                 arr[j]=1;
             }
-        }
+        System.out.println(Arrays.toString(arr));
     }
 }
