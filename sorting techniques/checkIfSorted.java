27567495d20fca0112f235593ef3244c3def8833
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-11-07 17:44:06 +0530

    check if sorted

---
+++ sorting techniques/checkIfSorted.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class checkIfSorted {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter the element of the array :");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean isSorted=true;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                isSorted=false;
+                break;
+            }
+        }
+        System.out.println(isSorted);
+    }
+}
