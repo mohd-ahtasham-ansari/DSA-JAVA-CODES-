e6ba73fab3a069285327319e4495d9e98168969d
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-11-07 18:05:08 +0530

    check if sorted

--- sorting techniques/CheckIfSorted.java
+++ sorting techniques/CheckIfSorted.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class checkIfSorted {
+public class CheckIfSorted {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
